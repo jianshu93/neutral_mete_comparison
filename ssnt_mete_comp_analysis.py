967e96ee165f761d5a4f7dbc313c16e859f9cfa7
Xiao Xiao <xiao@weecology.org>
2015-11-13 00:10:06 -0500

    Clean up file.

--- ssnt_mete_comp_analysis.py
+++ ssnt_mete_comp_analysis.py
@@ -12,9 +12,10 @@ import multiprocessing
 dat_list = ['ACA', 'BCI', 'BVSF', 'CSIRO', 'FERP', 'Lahei', 'LaSelva', 'NC', 'Oosting', 'Serimbu', 
             'WesternGhats', 'Cocoli', 'Luquillo', 'Sherman', 'Shirakami']
 
-# Obtain pred-obs for three patterns * four models 
+# Obtain predicted-observed values for the three patterns from the four models 
 dat_list_keep = []
 dat_site_list = []
+model_list = ['ssnt_0', 'ssnt_1', 'asne', 'agsne']
 for dat_name in dat_list:
     dat = wk.import_raw_data('./data/' + dat_name + '.csv')
     for site in np.unique(dat['site']):
@@ -23,26 +24,23 @@ for dat_name in dat_list:
         if dat_clean is not None:
             dat_list_keep.append(dat_name)
             dat_site_list.append([dat_name, site])
-            
-            smc.get_obs_pred_sad(dat_clean, dat_name, 'ssnt')
-            smc.get_obs_pred_sad(dat_clean, dat_name, 'asne')
-            smc.get_obs_pred_sad(dat_clean, dat_name, 'agsne')
-            
-            smc.get_obs_pred_isd(dat_clean, dat_name, 'ssnt_0')
-            smc.get_obs_pred_isd(dat_clean, dat_name, 'ssnt_1')
-            smc.get_obs_pred_isd(dat_clean, dat_name, 'asne')
-            smc.get_obs_pred_isd(dat_clean, dat_name, 'agsne')            
-            
-            smc.get_obs_pred_sdr(dat_clean, dat_name, 'ssnt_0')
-            smc.get_obs_pred_sdr(dat_clean, dat_name, 'ssnt_1')
-            smc.get_obs_pred_sdr(dat_clean, dat_name, 'asne')
-            smc.get_obs_pred_sdr(dat_clean, dat_name, 'agsne')            
+            smc.get_lik_sp_abd_dbh_four_models(dat_clean, dat_name)
+           
+            for model in model_list:
+                if model is 'ssnt_0': smc.get_obs_pred_sad(dat_clean, dat_name, 'ssnt')
+                elif model in ['asne', 'agsne']: smc.get_obs_pred_sad(dat_clean, dat_name, model)
+                smc.get_obs_pred_isd(dat_clean, dat_name, model)
+                smc.get_obs_pred_sdr(dat_clean, dat_name, model)
 
-# Plot the predicted versus observed values for the three patterns and the four models
-smc.plot_obs_pred_four_models(dat_list)
+# Obtain and plot the log-likelihood comparisons (Fig.1)
+smc.plot_likelihood_comp()
+# Plot the predicted versus observed values (Fig. 2)
+smc.plot_obs_pred_four_models(dat_list_keep)
+# Plot the R^2 comparisons (Fig. 3)
+smc.plot_r2_comp(dat_site_list)
 
 # Boostrap analyses
-model_list = ['ssnt_0', 'ssnt_1', 'asne', 'agsne']
+# Caution: the bootstrap analyses can take days, depending on the size of the data sets
 for model in model_list:
     def model_boot_sad(name_site_combo):
         smc.bootstrap_SAD(name_site_combo, model, Niter = 500)
@@ -67,19 +65,6 @@ for model in model_list:
     pool.close()
     pool.join()
 
-# Plot results from bootstrap analysis for four models
+# Plot results from bootstrap analysis for four models (Figs B1 - B4)
 for model in model_list:
     smc.plot_bootstrap(dat_list, model)
-
-# Obtain and plot likelihood comparison among models
-dat_list_keep = []
-dat_site_list = []
-for dat_name in dat_list:
-    dat = wk.import_raw_data('./data/' + dat_name + '.csv')
-    for site in np.unique(dat['site']):
-        dat_site = dat[dat['site'] == site]
-        dat_clean = smc.clean_data_agsne(dat_site)
-        if dat_clean is not None:
-            smc.get_lik_sp_abd_dbh_four_models(dat_clean, dat_name)
-
-smc.plot_likelihood_comp()
